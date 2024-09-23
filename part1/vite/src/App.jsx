88a3a7a12935e116b74d001239add1353a34183c
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-23 18:02:31 +0300

    part0

--- part1/vite/src/App.jsx
+++ part1/vite/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-/*import Content from "./components/Content";
+import Content from "./components/Content";
 import Header from "./components/Header";
 import Total from "./components/Total";
 
@@ -33,22 +33,4 @@ const App = () => {
   );
 };
 
-export default App;*/
-const App = () => {
-  const [value, setValue] = useState(10)
-  
-  const setToValue = (newValue) => () => {
-    console.log('value now', newValue)  // print the new value to console
-    setValue(newValue)
-  }
-  
-  return (
-    <div>
-      {value}
-      <button onClick={setToValue(1000)}>thousand</button>
-      <button onClick={setToValue(0)}>reset</button>
-      <button onClick={setToValue(value + 1)}>increment</button>
-    </div>
-  )
-}
-export default App
+export default App;
