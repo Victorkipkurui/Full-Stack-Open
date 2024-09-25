809362e961d632e6cca08cec7bab1129cea40972
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-25 16:46:06 +0300

    part2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ search, handleSearch }) => {
   return (
@@ -29,29 +30,36 @@ const PersonForm = ({ newName, number, handleNameChange, handleNumberChange, add
 const Person = ({ persons }) => {
   return (
     <>
-    {persons.length > 0 ? (
-      persons.map((person) => (
-        <p key={person.id}>
-          {person.name} {person.number}
-        </p>
-      ))
-    ) : (
-      <p>No result found</p>
-    )}
-  </>
+      {persons.length > 0 ? (
+        persons.map((person) => (
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ))
+      ) : (
+        <p>No result found</p>
+      )}
+    </>
   );
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [number, setNumber] = useState('');
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/persons')
+      .then((response) => {
+        const data = response.data;
+        setPersons(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
